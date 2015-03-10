21d2af156ea646883e987d914b0917bb011864c9
Michael Meyer <m.meyer2k@gmail.com>
2015-03-10 14:53:36 -0500

    Update shim.php

--- shim.php
+++ shim.php
@@ -11,7 +11,29 @@
  * @license  http://opensource.org/licenses/MIT The MIT License (MIT)
  * @link     https://github.com/mmeyer2k/retro
  */
-
+ 
+/*
+ * Function to convert array to csv string
+ */
+if(!function_exists('str_putcsv'))
+{
+    function str_putcsv($input, $delimiter = ',', $enclosure = '"')
+    {
+        // Open a memory "file" for read/write...
+        $fp = fopen('php://temp', 'r+');
+        // ... write the $input array to the "file" using fputcsv()...
+        fputcsv($fp, $input, $delimiter, $enclosure);
+        // ... rewind the "file" so we can read what we just wrote...
+        rewind($fp);
+        // ... read the entire line into a variable...
+        $data = fread($fp, 1048576);
+        // ... close the "file"...
+        fclose($fp);
+        // ... and return the $data to the caller, with the trailing newline from fgets() removed.
+        return rtrim($data, "\n");
+    }
+ }
+ 
 /**
  * Shim to allow for hex2bin in less than PHP 5.4.
  * 
