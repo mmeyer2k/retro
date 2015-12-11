139ac5b055e0fa013a14a81267ea7be48bca4aa9
Michael M <m.meyer2k@gmail.com>
2015-12-11 16:02:04 -0600

    Update shim.php

--- shim.php
+++ shim.php
@@ -52,16 +52,21 @@ if (!function_exists('str_putcsv')) {
     {
         // Open a memory "file" for read/write...
         $fp = fopen('php://temp', 'r+');
+        
         // ... write the $input array to the "file" using fputcsv()...
         foreach ($input as $i) {
             fputcsv($fp, $i, $delimiter, $enclosure, $escape_char);
         }
+        
         // ... rewind the "file" so we can read what we just wrote...
         rewind($fp);
-        // ... read the entire line into a variable...
+        
+        // ... read the temp stream into a variable...
         $data = stream_get_contents($fp);
+        
         // ... close the "file"...
         fclose($fp);
+        
         // ... and return the $data to the caller, with the trailing newline from fgets() removed.
         return rtrim($data, "\n");
     }
